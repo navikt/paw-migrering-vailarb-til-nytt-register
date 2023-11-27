ec8fd39a883abbdbf6244069a2e3c03f0ead3b69
Nils Martin Sande <nils.martin.sande@nav.no>
2023-11-27 14:51:22 +0100

    Fjærnet ubrukt import

--- app/src/main/kotlin/no/nav/paw/migrering/app/MappingFunksjoner.kt
+++ app/src/main/kotlin/no/nav/paw/migrering/app/MappingFunksjoner.kt
@@ -15,7 +15,6 @@ import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.time.temporal.ChronoUnit
 import java.util.*
-import kotlin.Metadata
 
 
 fun tilPeriode(periode: ArbeidssokerperiodeHendelseMelding): Hendelse =
